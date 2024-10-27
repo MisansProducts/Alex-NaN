cdce8f338f86abcb8461a72899aadee3178a53e5
Alex Akoopie <alexakoopie@gmail.com>
2024-10-27 08:48:13 -0700

    Lighting pt. 1
    Added lighting. Added a background that actually works this time. Made the lighting linked with the score. Need to bake shadows properly.

--- Assets/Scripts/GameScript.cs
+++ Assets/Scripts/GameScript.cs
@@ -1,13 +1,16 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.Rendering.Universal;
 
 public class GameScript : MonoBehaviour {
     public GameObject player;
     public GameObject floorSpawner;
+    public Light2D spotLight;
     [SerializeField] public float gameSpeed = 5f;
     [SerializeField] public float spikeChance = 0.2f;
     public int currentScore = 0;
     public int highScore = 0;
+    private const float spotLightRedScore = 200f; // Score in which spotlight turns fully red
 
     // UI Text references
     public TextMeshProUGUI currentScoreText;
@@ -18,6 +21,7 @@ public class GameScript : MonoBehaviour {
         highScore = currentScore > highScore ? currentScore : highScore;
         currentScoreText.text = "Score: " + currentScore.ToString();
         highScoreText.text = "Hi-Score: " + highScore.ToString();
+        spotLight.color = Color.HSVToRGB(0, currentScore / spotLightRedScore * 1f, 1f);
     }
 
     public void GameOver() {
@@ -27,6 +31,7 @@ public class GameScript : MonoBehaviour {
         }
         gameSpeed = 5f;
         currentScore = 0;
+        spotLight.color = Color.HSVToRGB(0, 0, 1f);
 
         Instantiate(player, new Vector3(-6.5f, 0.5f, 0), Quaternion.identity, transform);
         Instantiate(floorSpawner, new Vector3(0, 0, 0), Quaternion.identity, transform);
@@ -34,6 +39,7 @@ public class GameScript : MonoBehaviour {
     
     // Start is called before the first frame update
     void Start() {
+        spotLight = GameObject.FindGameObjectWithTag("Light").GetComponent<Light2D>();
         Instantiate(player, new Vector3(-6.5f, 0.5f, 0), Quaternion.identity, transform);
         Instantiate(floorSpawner, new Vector3(0, 0, 0), Quaternion.identity, transform);
     }
