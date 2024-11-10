9bcd3c95413dc7574f3b672af6e01591c7eee975
voxelit <noxeleria@gmail.com>
2024-11-09 17:35:35 -0800

    Platforms now change with current elevation
    Platforms are now fixed to be 2 cells higher than the current elevation and are fixed to 5 cells in length.

--- Assets/Scripts/PlatformSpawner.cs
+++ Assets/Scripts/PlatformSpawner.cs
@@ -13,6 +13,7 @@ public class platformSpawnerScript : MonoBehaviour {
     
     // Variables
     private int spikeCoolDown = 2;
+    private int platCoolDown = 0;
     private int platLength = 5;
     private int platHeight = 3;
     private const float Y = 0.5f;
@@ -20,48 +21,55 @@ public class platformSpawnerScript : MonoBehaviour {
     private const int rightEdge = 17; // 16 + 1 padding
 
     // Function to spawn floors
-    void SpawnPlatform(float X = 8f, bool first = true, int floorHeight = 1) {
+    void SpawnPlatform(float X = 8f, bool first = true) {
+        Debug.Log("dxghk");
         last = Instantiate(platform, new Vector3(X, Y, 0), Quaternion.identity, transform).transform;
-        platHeight = floorHeight + 2;
 
         // Fills floor with cell prefabs
         // 21 to 30
         // create object ? for platform : ternary
-        for (int x = 0; x < platLength; x++) {
-                Vector3 cellPosition = new Vector3(x - platLength / 2, platHeight, 0); // (floorLength / 2) is floor center
-                GameObject cellPrefab = Instantiate(cell, cellPosition, Quaternion.identity, last);
-                cellPrefab.transform.localPosition = cellPosition; // Local to last
+        if(platCoolDown >= 5) {
+            for (int x = 0; x < platLength; x++) {
+                platHeight = floorScript.getFloorHeight() + 2;
+                    Vector3 cellPosition = new Vector3(x - platLength / 2, platHeight, 0); // (floorLength / 2) is floor center
+                    GameObject cellPrefab = Instantiate(cell, cellPosition, Quaternion.identity, last);
+                    cellPrefab.transform.localPosition = cellPosition; // Local to last
 
 
-            // =-=-=Spike Generation=-=-=
-            if (first || (gameScript.spotLightScore - gameScript.currentScore > 0 && gameScript.spotLightScore - gameScript.currentScore < 20)) continue; // skips spawning spikes
-            if (spikeCoolDown == 2) {
-                // Randomly spawns spikes
-                if (Random.value <= gameScript.spikeChance) {
-                    Vector3 spikePosition = new Vector3(x - 0.5f - platLength / 2, platLength, 0);
-                    float randomSpike = Random.value;
-                    GameObject spikePrefab;
-                    if (randomSpike <= 1f/2f) {
-                        spikePrefab = Instantiate(singleSpike, spikePosition, Quaternion.identity, last);
-                        spikeCoolDown = 0;
-                        spikePrefab.transform.localPosition = spikePosition;
-                    }
-                    else if (randomSpike <= 5f/6f) {
-                        if (x >= platLength - 1) continue;
-                        spikePrefab = Instantiate(doubleSpike, spikePosition, Quaternion.identity, last);
-                        spikeCoolDown = -1;
-                        spikePrefab.transform.localPosition = spikePosition;
-                    }
-                    else if (x < platLength - 2) {
-                        spikePrefab = Instantiate(tripleSpike, spikePosition, Quaternion.identity, last);
-                        spikeCoolDown = -2;
-                        spikePrefab.transform.localPosition = spikePosition;
+                // =-=-=Spike Generation=-=-=
+                if (first || (gameScript.spotLightScore - gameScript.currentScore > 0 && gameScript.spotLightScore - gameScript.currentScore < 20)) continue; // skips spawning spikes
+                if (spikeCoolDown == 2) {
+                    // Randomly spawns spikes
+                    if (Random.value <= gameScript.spikeChance) {
+                        Vector3 spikePosition = new Vector3(x - 0.5f - platLength / 2, platHeight+1, 0);
+                        float randomSpike = Random.value;
+                        GameObject spikePrefab;
+                        if (randomSpike <= 1f/2f) {
+                            spikePrefab = Instantiate(singleSpike, spikePosition, Quaternion.identity, last);
+                            spikeCoolDown = 0;
+                            spikePrefab.transform.localPosition = spikePosition;
+                        }
+                        else if (randomSpike <= 5f/6f) {
+                            if (x >= platLength - 1) continue;
+                            spikePrefab = Instantiate(doubleSpike, spikePosition, Quaternion.identity, last);
+                            spikeCoolDown = -1;
+                            spikePrefab.transform.localPosition = spikePosition;
+                        }
+                        else if (x < platLength - 2) {
+                            spikePrefab = Instantiate(tripleSpike, spikePosition, Quaternion.identity, last);
+                            spikeCoolDown = -2;
+                            spikePrefab.transform.localPosition = spikePosition;
+                        }
                     }
                 }
+                else { // just some basic logic to make the game fair
+                    spikeCoolDown++;
+                }
             }
-            else { // just some basic logic to make the game fair
-                spikeCoolDown++;
-            }
+            platCoolDown = 0;
+        }
+        else {
+            platCoolDown++;
         }
     }
 
@@ -87,13 +95,8 @@ public class platformSpawnerScript : MonoBehaviour {
             Destroy(child.gameObject);
         }
 
-        // if (last.position.x + platLength / 2 < rightEdge)
-        // SpawnPlatform(last.position.x + platLength, false);
-
-        // Random chance of spawning platforms
-        if (Random.Range(0,5) == 0) {
-            int floorHeight = floorScript.getFloorHeight();
-            SpawnPlatform(last.position.x + platLength, false, floorHeight);
-        }
+        if (last.position.x + platLength / 2 < rightEdge)
+            SpawnPlatform(last.position.x + platLength, false);
     }
+
 }
