3a9180a212a4fdf5a883a6d8f06b85cd7a3eefc3
voxelit <noxeleria@gmail.com>
2024-11-17 19:02:27 -0800

    Changed floor variability for length and height
    Floors now spawn at a random length between 5 and 15 at a Y-value of 1-5. This will naturally create pitfalls.

--- Assets/Scripts/FloorSpawnerScript.cs
+++ Assets/Scripts/FloorSpawnerScript.cs
@@ -15,7 +15,7 @@ public class FloorSpawnerScript : MonoBehaviour {
     [SerializeField] private GameObject battery;
     
     // Variables
-    private const int floorLength = 25;
+    private int floorLength = 25;
     private const float Y = 0.5f;
     private const int leftEdge = -1; // 0 - 1 padding
     private const int rightEdge = 17; // 16 + 1 padding
@@ -23,8 +23,9 @@ public class FloorSpawnerScript : MonoBehaviour {
     // Function to spawn floors
     private void SpawnFloor(float X = 8f, bool first = true) {
         print("WOW! A FLOOR!!!");
-        last = Instantiate(floor, new Vector3(X, Y, 0), Quaternion.identity, transform).transform;
-        last.localScale = new Vector3(25, 1, 1); // edits floor's scale
+        floorLength = Random.Range(5,15);
+        last = Instantiate(floor, new Vector3(X, Random.Range(1,5), 0), Quaternion.identity, transform).transform;
+        last.localScale = new Vector3(floorLength, 1, 1); // edits floor's scale
         last.GetComponent<Renderer>().material.color = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f); // random color for testing
         
     }
@@ -51,9 +52,12 @@ public class FloorSpawnerScript : MonoBehaviour {
             child.Translate(Vector3.left * gameScript.gameSpeed * Time.deltaTime);
 
             // Deletes floors outside of the playable area
-            if (child.position.x + floorLength / 2 < leftEdge)
+            if (child.position.x + 15 < leftEdge)
                 Destroy(child.gameObject);
         }
+
+        //Maybe useful for a different game mode
+        //floorLength = Random.Range(5,15);
 
         if (last.position.x + floorLength / 2 < rightEdge){
             SpawnFloor(last.position.x + floorLength, false);
