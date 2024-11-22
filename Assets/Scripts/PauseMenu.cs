11080494b1a2354fd4803bbada7f84317e42c0ab
kyomi <87290470+koyomi7@users.noreply.github.com>
2024-11-22 05:46:10 -0800

    added foundations of pause menu and main menu
    either click on the pause button or esc key on keyboard to pause and un-pause the game, it will have a three seconds countdown before resuming. Setting menu is not done and needs to be added.

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject pauseMenu;
+    [SerializeField] private TextMeshProUGUI countdownText;
+
+    private bool isPaused = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void PauseGame()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0;
+        isPaused = true;
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenu.SetActive(false);
+        StartCoroutine(WaitAndResume());
+    }
+
+    private IEnumerator WaitAndResume()
+    {
+        countdownText.gameObject.SetActive(true);
+        for (int i = 3; i > 0; i--)
+        {
+            countdownText.text = i.ToString();
+            yield return new WaitForSecondsRealtime(1);
+        }
+        countdownText.gameObject.SetActive(false);
+        Time.timeScale = 1;
+        isPaused = false;
+    }
+
+    public void HomeButton()
+    {
+        SceneManager.LoadSceneAsync(0);
+        Time.timeScale = 1;
+    }
+}
