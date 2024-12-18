92c395bf8c02437a760a3cc55a61810a2c4a60be
Alex Akoopie <alexakoopie@gmail.com>
2024-12-17 20:40:18 -0800

    Code review: FloorSpawnerScript.cs
    Abstracted the child instantiation in the GenerateStructure() function, got rid of a goto statement, and cleaned up some logic.

--- Assets/Scripts/FloorSpawnerScript.cs
+++ Assets/Scripts/FloorSpawnerScript.cs
@@ -11,7 +11,6 @@ public class FloorSpawnerScript : MonoBehaviour {
     [SerializeField] private GameObject cell; // 1x1 cell prefab
     [SerializeField] private GameObject singleSpike, doubleSpike, tripleSpike; // Hazards
     [SerializeField] private GameObject battery;
-    // Add new powerup prefabs
     [SerializeField] private GameObject fuelPrefab; // Adds maxjumptime 
     [SerializeField] private GameObject shieldPrefab; // Adds invincibility for one hit
     [SerializeField] private GameObject extraJumpPrefab; // Adds an extra jump
@@ -32,42 +31,54 @@ public class FloorSpawnerScript : MonoBehaviour {
     private int lastLength = 25;
     private const int leftEdge = -1; // 0 - 1 padding
     private const int rightEdge = 17; // 16 + 1 padding
-    private int floorNumber = 1; // for debugging
     private int spikeCoolDownIt = 0; // iterator for hazard cooldown
     private bool spawnRandomPowerup = false;
     private float powerupTimer;
     private int maxFuelCount = 3;
     private int maxExtraJumpCount = 1;
 
+    // Function to abstract the child instaniation process
+    private void CreateInstanceHelper(GameObject prefab, Vector3 position) {
+        // Creates prefab instance
+        prefab = Instantiate(prefab, position, Quaternion.identity);
+
+        // Saves the previous scale
+        Vector3 prefabScale = prefab.transform.localScale;
+        float prefabX = prefabScale.x;
+        float prefabY = prefabScale.y;
+        float prefabZ = prefabScale.z;
+
+        // Attaches prefab to the last floor
+        prefab.transform.SetParent(last, true);
+        prefab.transform.localScale = new Vector3(prefabX / last.localScale.x, prefabY / last.localScale.y, prefabZ / last.localScale.z);
+    }
+
     // Function to generate structure
     private void GenerateStructure(float X, int Y, bool first) {
         int newLastLength = lastLength; // Local to hazard generation
-        if (first) goto cellGeneration; // skips hole gen on first floor
+
         // =-=-=Hole Generation=-=-=
-        if (Random.value <= gameScript.holeChance) {
+        if (!first && Random.value <= gameScript.holeChance) {
             newLastLength = lastLength - 3; // 3 wide holes
             last.localScale = new Vector3(newLastLength, 1, 1);
         }
+        
         // =-=-=Main Structure Generation Loop=-=-=
-        cellGeneration:
         for (int i = 0; i < newLastLength; i++) {
             // =-=-=Cell Generation=-=-=
-            Vector3 cellPosition = new Vector3(X + i, Y, 0);
-            GameObject cellPrefab = Instantiate(cell, cellPosition, Quaternion.identity);
-            cellPrefab.transform.SetParent(last, true);
-            cellPrefab.transform.localScale = new Vector3(1f / last.localScale.x, 1f / last.localScale.y, 1f / last.localScale.z);
-            if (first) continue; // skips hazard gen on first floor
+            CreateInstanceHelper(cell, new Vector3(X + i, Y, 0));
+            if (first) continue; // only generate cells for the first floor
+
             // =-=-=Spike Generation=-=-=
             if (spikeCoolDownIt == gameScript.spikeCoolDown) {
                 // Randomly spawns spikes
                 if (Random.value <= gameScript.spikeChance && !spawnRandomPowerup) { // !spawnRandomPowerup do not spawn hazards when powerup should spawn
                     Vector3 spikePosition = new Vector3(X + i, Y + 1, 0);
-                    GameObject spikePrefab = null;
                     // Randomly chooses which type of spike to spawn
                     float randomSpike = Random.value;
                     retrySpike:
                     if (randomSpike <= gameScript.singleSpikeChance) { // Single
-                        spikePrefab = Instantiate(singleSpike, spikePosition, Quaternion.identity);
+                        CreateInstanceHelper(singleSpike, spikePosition);
                         spikeCoolDownIt = 0;
                     }
                     else if (randomSpike <= gameScript.singleSpikeChance + gameScript.doubleSpikeChance) { // Double
@@ -75,50 +86,44 @@ public class FloorSpawnerScript : MonoBehaviour {
                             randomSpike -= gameScript.doubleSpikeChance;
                             goto retrySpike;
                         }
-                        spikePrefab = Instantiate(doubleSpike, spikePosition, Quaternion.identity);
+                        CreateInstanceHelper(doubleSpike, spikePosition);
                         spikeCoolDownIt = -1;
                     }
-                    else if (randomSpike > gameScript.singleSpikeChance + gameScript.doubleSpikeChance) { // Triple
+                    else { // Triple
                         if (i + 2 >= newLastLength) { // Fixes overhanging spikes; tries for doubleSpike
                             randomSpike -= gameScript.tripleSpikeChance;
                             goto retrySpike;
                         }
-                        spikePrefab = Instantiate(tripleSpike, spikePosition, Quaternion.identity);
+                        CreateInstanceHelper(tripleSpike, spikePosition);
                         spikeCoolDownIt = -2;
-                    }
-                    if (spikePrefab != null) {
-                        spikePrefab.transform.SetParent(last, true);
-                        spikePrefab.transform.localScale = new Vector3(1f / last.localScale.x, 1f / last.localScale.y, 1f / last.localScale.z);
                     }
                 }
             }
             else spikeCoolDownIt++;
+
             // =-=-=Powerup Generation=-=-=
             if (spawnRandomPowerup) {
                 if (spikeCoolDownIt < 0) continue; // cannot spawn powerups on hazards
                 Vector3 powerupPosition = new Vector3(X + i, Y + 1, 0);
-                GameObject powerupInstance = null;
                 // Randomly choose a powerup to spawn
                 switch (Random.Range(0, 3)) {
                     case 0: // Fuel
                         if (playerScript.fuelCount < maxFuelCount) {
-                            powerupInstance = Instantiate(fuelPrefab, powerupPosition, Quaternion.identity);
+                            CreateInstanceHelper(fuelPrefab, powerupPosition);
+                            break;
                         }
-                        break;
+                        goto case 1;
                     case 1: // ExtraJump
                         if (playerScript.extraJumpCount < maxExtraJumpCount) {
-                            powerupInstance = Instantiate(extraJumpPrefab, powerupPosition, Quaternion.identity);
+                            CreateInstanceHelper(extraJumpPrefab, powerupPosition);
+                            break;
                         }
-                        break;
+                        goto case 2;
                     case 2: // Shield
-                        powerupInstance = Instantiate(shieldPrefab, powerupPosition, Quaternion.identity);
+                        CreateInstanceHelper(shieldPrefab, powerupPosition);
                         break;
                 }
-                if (powerupInstance != null) {
-                    powerupInstance.transform.SetParent(last, true);
-                    powerupInstance.transform.localScale = new Vector3(0.5f / last.localScale.x, 0.5f / last.localScale.y, 0.5f / last.localScale.z);
-                    spawnRandomPowerup = false;
-                }
+                spawnRandomPowerup = false;
             }
         }
     }
@@ -135,12 +140,9 @@ public class FloorSpawnerScript : MonoBehaviour {
 
     // Function to spawn floors
     private void SpawnFloor(float X = 0f, int Y = 0, bool first = true) {
-        print($"Floor number: {floorNumber}"); // for debugging
-        floorNumber++;
         lastLength = first ? 25 : Random.Range(5, 25);
         last = Instantiate(floor, new Vector3(X, Y, 0), Quaternion.identity, transform).transform;
         last.localScale = new Vector3(lastLength, 1, 1); // edits floor's scale
-        last.GetComponent<Renderer>().material.color = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f); // random color for testing
         GenerateStructure(X, Y, first); // generates structure
         floorLengths.Enqueue(new FloorMeta(last, lastLength));
     }
@@ -148,10 +150,7 @@ public class FloorSpawnerScript : MonoBehaviour {
     // Awake is called before the game starts; used to initialize object references
     void Awake() {
         gameScript = FindObjectOfType<GameScript>();
-        GameObject playerObject = GameObject.FindWithTag("Player");
-        if (playerObject != null) {
-            playerScript = playerObject.GetComponent<PlayerScript>();
-        }
+        playerScript = GameObject.FindWithTag("Player").GetComponent<PlayerScript>();
     }
 
     // Start is called before the first frame update
