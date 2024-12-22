3f6d6cdd819a0301f3ef966f04eeacbb1a31a9aa
Alex Akoopie <alexakoopie@gmail.com>
2024-12-21 22:16:32 -0800

    Reimplemented platforms
    Added basic platform spawning with some convoluted logic in the floor spawner script. I accounted for all possible overlaps except for a platform and the next floor spawning next to each other on the same Y level.

--- Assets/Scripts/FloorSpawnerScript.cs
+++ Assets/Scripts/FloorSpawnerScript.cs
@@ -29,16 +29,19 @@ public class FloorSpawnerScript : MonoBehaviour {
     private Queue<FloorMeta> floorLengths = new Queue<FloorMeta>();
     private FloorMeta firstFloor;
     private int lastLength = 25;
+    private int lastValidY = 5; // Where a floor can spawn without intersecting a platform
+    private int lastValidX = 0; // Where a platform can spawn without intersecting another platform
     private const int leftEdge = -1; // 0 - 1 padding
     private const int rightEdge = 17; // 16 + 1 padding
     private int spikeCoolDownIt = 0; // iterator for hazard cooldown
+    private int spikeCoolDownItPlat = 0; // hazard cooldown iterator for platforms
     private bool spawnRandomPowerup = false;
     private float powerupTimer;
     private int maxFuelCount = 3;
     private int maxExtraJumpCount = 1;
 
     // Function to abstract the child instaniation process
-    private void CreateInstanceHelper(GameObject prefab, Vector3 position) {
+    private void CreateInstanceHelper(Transform parent, GameObject prefab, Vector3 position) {
         // Creates prefab instance
         prefab = Instantiate(prefab, position, Quaternion.identity);
 
@@ -49,28 +52,28 @@ public class FloorSpawnerScript : MonoBehaviour {
         float prefabZ = prefabScale.z;
 
         // Attaches prefab to the last floor
-        prefab.transform.SetParent(last, true);
-        prefab.transform.localScale = new Vector3(prefabX / last.localScale.x, prefabY / last.localScale.y, prefabZ / last.localScale.z);
+        prefab.transform.SetParent(parent, true);
+        prefab.transform.localScale = new Vector3(prefabX / parent.localScale.x, prefabY / parent.localScale.y, prefabZ / parent.localScale.z);
     }
 
     // Function to generate structure
-    private void GenerateStructure(float X, int Y, bool first) {
-        int newLastLength = lastLength; // Local to hazard generation
+    private void GenerateStructure(Transform currFloor, int currFloorLength, float X, int Y, bool first, bool isPlatform = false) {
+        int newLength = currFloorLength; // Local to hazard generation
 
         // =-=-=Hole Generation=-=-=
-        if (!first && Random.value <= gameScript.holeChance) {
-            newLastLength = lastLength - 3; // 3 wide holes
-            last.localScale = new Vector3(newLastLength, 1, 1);
+        if (!first && !isPlatform && Random.value <= gameScript.holeChance) {
+            newLength = currFloorLength - 3; // 3 wide holes
+            currFloor.localScale = new Vector3(newLength, 1, 1);
         }
         
         // =-=-=Main Structure Generation Loop=-=-=
-        for (int i = 0; i < newLastLength; i++) {
+        for (int i = 0; i < newLength; i++) {
             // =-=-=Cell Generation=-=-=
-            CreateInstanceHelper(cell, new Vector3(X + i, Y, 0));
+            CreateInstanceHelper(currFloor, cell, new Vector3(X + i, Y, 0));
             if (first) continue; // only generate cells for the first floor
 
             // =-=-=Spike Generation=-=-=
-            if (spikeCoolDownIt == gameScript.spikeCoolDown) {
+            if ((!isPlatform && spikeCoolDownIt == gameScript.spikeCoolDown) || (isPlatform && spikeCoolDownItPlat == gameScript.spikeCoolDown)) {
                 // Randomly spawns spikes
                 if (Random.value <= gameScript.spikeChance && !spawnRandomPowerup) { // !spawnRandomPowerup do not spawn hazards when powerup should spawn
                     Vector3 spikePosition = new Vector3(X + i, Y + 1, 0);
@@ -78,53 +81,59 @@ public class FloorSpawnerScript : MonoBehaviour {
                     float randomSpike = Random.value;
                     retrySpike:
                     if (randomSpike <= gameScript.singleSpikeChance) { // Single
-                        CreateInstanceHelper(singleSpike, spikePosition);
-                        spikeCoolDownIt = 0;
+                        CreateInstanceHelper(currFloor, singleSpike, spikePosition);
+                        if (!isPlatform) spikeCoolDownIt = 0;
+                        else spikeCoolDownItPlat = 0;
                     }
                     else if (randomSpike <= gameScript.singleSpikeChance + gameScript.doubleSpikeChance) { // Double
-                        if (i + 1 >= newLastLength) { // Fixes overhanging spikes; guaranteed singleSpike
+                        if (i + 1 >= newLength) { // Fixes overhanging spikes; guaranteed singleSpike
                             randomSpike -= gameScript.doubleSpikeChance;
                             goto retrySpike;
                         }
-                        CreateInstanceHelper(doubleSpike, spikePosition);
-                        spikeCoolDownIt = -1;
+                        CreateInstanceHelper(currFloor, doubleSpike, spikePosition);
+                        if (!isPlatform) spikeCoolDownIt = -1;
+                        else spikeCoolDownItPlat = -1;
                     }
                     else { // Triple
-                        if (i + 2 >= newLastLength) { // Fixes overhanging spikes; tries for doubleSpike
+                        if (i + 2 >= newLength) { // Fixes overhanging spikes; tries for doubleSpike
                             randomSpike -= gameScript.tripleSpikeChance;
                             goto retrySpike;
                         }
-                        CreateInstanceHelper(tripleSpike, spikePosition);
-                        spikeCoolDownIt = -2;
+                        CreateInstanceHelper(currFloor, tripleSpike, spikePosition);
+                        if (!isPlatform) spikeCoolDownIt = -2;
+                        else spikeCoolDownItPlat = -2;
                     }
                 }
             }
-            else spikeCoolDownIt++;
+            else {
+                if (!isPlatform) spikeCoolDownIt++;
+                else spikeCoolDownItPlat++;
+            }
 
             // =-=-=Powerup Generation=-=-=
             if (spawnRandomPowerup) {
-                if (spikeCoolDownIt < 0) continue; // cannot spawn powerups on hazards
+                if ((!isPlatform && spikeCoolDownIt < 0) || (isPlatform && spikeCoolDownItPlat < 0)) continue; // cannot spawn powerups on hazards
                 Vector3 powerupPosition = new Vector3(X + i, Y + 1, 0);
                 // Randomly choose a powerup to spawn
                 switch (Random.Range(0, 4)) {
                     case 0: // Fuel
                         if (playerScript.fuelCount < maxFuelCount) {
-                            CreateInstanceHelper(fuelPrefab, powerupPosition);
+                            CreateInstanceHelper(currFloor, fuelPrefab, powerupPosition);
                             break;
                         }
                         goto case 3; // skips to battery spawn
                     case 1: // ExtraJump
                         if (playerScript.extraJumpCount < maxExtraJumpCount) {
-                            CreateInstanceHelper(extraJumpPrefab, powerupPosition);
+                            CreateInstanceHelper(currFloor, extraJumpPrefab, powerupPosition);
                             break;
                         }
                         goto case 3; // skips to battery spawn
                     case 2: // Shield
-                        CreateInstanceHelper(shieldPrefab, powerupPosition);
+                        CreateInstanceHelper(currFloor, shieldPrefab, powerupPosition);
                         break;
                     case 3: // Battery
                         if (gameScript.mode2) {
-                            CreateInstanceHelper(battery, powerupPosition);
+                            CreateInstanceHelper(currFloor, battery, powerupPosition);
                             break;
                         }
                         goto case 2; // must be shield spawn if not mode 2
@@ -146,11 +155,29 @@ public class FloorSpawnerScript : MonoBehaviour {
 
     // Function to spawn floors
     private void SpawnFloor(float X = 0f, int Y = 0, bool first = true) {
-        lastLength = first ? 25 : Random.Range(5, 25);
+        lastLength = first ? 25 : Random.Range(5, 26);
         last = Instantiate(floor, new Vector3(X, Y, 0), Quaternion.identity, transform).transform;
         last.localScale = new Vector3(lastLength, 1, 1); // edits floor's scale
-        GenerateStructure(X, Y, first); // generates structure
+        GenerateStructure(last, lastLength, X, Y, first); // generates structure
         floorLengths.Enqueue(new FloorMeta(last, lastLength));
+        // Spawns platform
+        if (!first && Random.value <= 0.5f) {
+            int randOffset = Random.Range(lastValidX, lastLength);
+            float platformX = X + randOffset;
+            int platformY = Y + Random.Range(3, 5);
+            lastValidY = platformY - (platformY - Y) + 1;
+            int platformLength = 5;
+            lastValidX = (lastLength - 5 - randOffset < 0) ? 5 : 0;
+            Transform platform = Instantiate(floor, new Vector3(platformX, platformY, 0), Quaternion.identity, transform).transform;
+            platform.name = "Platform(Clone)";
+            platform.localScale = new Vector3(platformLength, 1, 1);
+            GenerateStructure(platform, platformLength, platformX, platformY, first, true);
+            floorLengths.Enqueue(new FloorMeta(platform, platformLength));
+        }
+        else {
+            lastValidY = 5; // Resets floor Y diversity
+            lastValidX = 0; // Resets platform X restriction
+        }
     }
 
     // Awake is called before the game starts; used to initialize object references
@@ -191,7 +218,7 @@ public class FloorSpawnerScript : MonoBehaviour {
 
         // Creates floors outside of the playable area
         if (last.position.x + lastLength < rightEdge)
-            SpawnFloor(last.position.x + lastLength, Random.Range(1, 5), false);
+            SpawnFloor(last.position.x + lastLength, Random.Range(1, lastValidY), false);
         
         // Deletes floors outside of the playable area
         if (firstFloor.Floor.position.x + firstFloor.Length < leftEdge) {
