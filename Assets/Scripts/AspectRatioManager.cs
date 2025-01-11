25f873469812ebdeccc2df68b8ed70b7881529f4
Alex Akoopie <alexakoopie@gmail.com>
2025-01-11 15:08:36 -0800

    Fixed aspect ratio glitch
    Finally fixed the aspect ratio being inconsistent across different devices and window resizing, which showed stuff the player shouldn't see. Now it stretches and shows black bars like a normal video game would.

--- Assets/Scripts/AspectRatioManager.cs
+++ Assets/Scripts/AspectRatioManager.cs
@@ -1,32 +1,46 @@
 using UnityEngine;
 
-public class AspectRatioManager : MonoBehaviour
+public class AspectRatioManager : MonoBehaviour 
 {
-    public float targetAspect = 16f / 9f;
-
-    private void Start()
-    {
-        UpdateAspectRatio();
+    public Camera cam;
+    public float targetAspect = 16.0f / 9.0f;
+    private int lastWidth;
+    private int lastHeight;
+    
+    void Start() {
+        cam = GetComponent<Camera>();
+        lastWidth = Screen.width;
+        lastHeight = Screen.height;
+        ApplyLetterbox();
     }
 
-    private void UpdateAspectRatio()
-    {
-        float windowAspect = (float)Screen.width / Screen.height;
-        float scaleHeight = windowAspect / targetAspect;
-
-        if (scaleHeight < 1.0f) 
-        {
-            Camera.main.rect = new Rect((1.0f - scaleHeight) / 2.0f, 0.0f, scaleHeight, 1.0f);
-        }
-        else // Add black bars on the top and bottom
-        {
-            float scaleWidth = 1.0f / scaleHeight;
-            Camera.main.rect = new Rect(0.0f, (1.0f - scaleWidth) / 2.0f, 1.0f, scaleWidth);
+    void Update() {
+        if (Screen.width != lastWidth || Screen.height != lastHeight) {
+            lastWidth = Screen.width;
+            lastHeight = Screen.height;
+            ApplyLetterbox();
         }
     }
 
-    private void Update()
-    {
-        UpdateAspectRatio();
+    void ApplyLetterbox() {
+        float currentAspect = (float)Screen.width / Screen.height;
+        float bgWidth = 1.0f;
+        float bgHeight = 1.0f;
+
+        if (currentAspect > targetAspect) {
+            bgWidth = targetAspect / currentAspect;
+            bgHeight = 1.0f;
+        }
+        else {
+            bgWidth = 1.0f;
+            bgHeight = currentAspect / targetAspect;
+        }
+
+        cam.rect = new Rect(
+            (1.0f - bgWidth) / 2.0f,
+            (1.0f - bgHeight) / 2.0f,
+            bgWidth,
+            bgHeight
+        );
     }
 }
